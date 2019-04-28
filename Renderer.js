be62d73dd173b576c29adae30bc96db52e4ea64e
Ravid Cohen <ravidd9@gmail.com>
2019-04-28 11:03:01 +0300

    kinda finished..

--- Renderer.js
+++ Renderer.js
@@ -1,6 +1,9 @@
 class Renderer {
     renderBoard(board) {
         $("#container").empty()
+        let message = `<div id="message">Gold Rush</div>`
+        $("#container").append(message)
+        $("#message").css("display", "none")
         $("#container").css("grid-template-columns", `repeat(${board[0].length},1fr)`)
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
@@ -33,18 +36,35 @@ class Renderer {
 
     renderClock(score) {
         let secs = 10
-        const hi = () => {$("#clock").text() = "hi"}
+        const hi = function () {
+            if (secs > 1) {
+                secs--
+                $("#clock").text(secs)
+            }
+            else{
+                let message
+                score["1"] > score["2"] ? message = "Player 1 Won!" : null
+                score["2"] > score["1"] ? message = "Player 2 Won!" : null
+                score["1"] == score["2"] ? message = "Draw!" : null
+                $(".box").css("display", "none")
+                $("#message").css("display", "block")
+                $("#message").text(message)
+                clearInterval(timer)
+            }
+            
+        }
         let timer = setInterval(hi, 1000)
-        setTimeout(function () {
-            let message
-            score["1"] > score["2"] ? message = "Player 1 Won!" : null
-            score["2"] > score["1"] ? message = "Player 2 Won!" : null
-            score["1"] == score["2"] ? message = "Draw!" : null
-            $(".box").css("display", "none")
-            $("#message").css("display", "block")
-            $("#message").text() = message
 
-        }, 10000)
+        // setTimeout(function () {
+        //     let message
+        //     score["1"] > score["2"] ? message = "Player 1 Won!" : null
+        //     score["2"] > score["1"] ? message = "Player 2 Won!" : null
+        //     score["1"] == score["2"] ? message = "Draw!" : null
+        //     $(".box").css("display", "none")
+        //     $("#message").css("display", "block")
+        //     $("#message").text() = message
+
+        // }, 10000)
     }
 
     render(goldRush) {
